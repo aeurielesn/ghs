34959ebb630d2ecb6a72008ca3264f8fa07ea19d
Alexander Urieles <aeurielesn@gmail.com>
2014-12-03 22:35:06 +0100

    Add exception handling and configuration file

--- ghs.py
+++ ghs.py
@@ -7,29 +7,34 @@ import time
 import calendar
 import yaml
 import HTMLParser
+import sys
+import os
+from ConfigParser import SafeConfigParser
 
-subreddit_name = ""
-calendar_id = ""
-calendar_key = ""
+def get_config():
+  cfg = SafeConfigParser()
+  cfg_path = os.path.abspath(os.path.dirname(sys.argv[0]))
+  cfg_path = os.path.join(cfg_path, 'ghs.cfg')
+  cfg.read(cfg_path)
+  return cfg
 
 def now():
   return time.strftime("%Y-%m-%dT%H:%M:%SZ", time.gmtime())
 
-def events():
+def get_upcoming_events(calendar_id, calendar_token):
   r = requests.get("https://www.googleapis.com/calendar/v3/calendars/" + calendar_id + "/events", params={
     "maxResults": 5,
     "orderBy": "startTime",
     "singleEvents": "true",
     "fields": "items(location,start,summary,description),summary",
     "timeMin": now(),
-    "key": calendar_key
+    "key": calendar_token
   })
   
   for item in r.json()["items"]:
     yield item
 
-def timestamp(event):
-  timestamp
+def format_timestamp(event):
   current_time = time.time()
   event_time = calendar.timegm(time.strptime(event["start"]["dateTime"], "%Y-%m-%dT%H:%M:%SZ"))
 
@@ -46,7 +51,7 @@ def timestamp(event):
 
     return " ".join([format_segment(days, "d"), format_segment(hours, "h"), format_segment(minutes, "m")]).strip()
 
-def name(event):
+def format_event(event):
   name_md = u""
   properties = {}
 
@@ -74,23 +79,43 @@ def name(event):
   
   return name_md
 
-h = HTMLParser.HTMLParser()
-r = praw.Reddit(user_agent="ghs/2.0", site_name="oauth")
-r.login()
 
-while True:
-  print "fetching calendar data"
-  events_md = ""
-  for event in events():
-    events_md += "* %s  \n  %s\n\n" % (timestamp(event), name(event))
+def main():
+  cfg = get_config()
+  h = HTMLParser.HTMLParser()
 
-  print "fetching sidebar template"
-  subreddit = r.get_subreddit(subreddit_name)
-  template_md = h.unescape(subreddit.get_wiki_page("sidebar").content_md)
+  while True:
+    try:
+      r = praw.Reddit(user_agent="ghs/2.0")
+      r.set_oauth_app_info(client_id=cfg.get('reddit', 'oauth_client_id'),
+        client_secret=cfg.get('reddit', 'oauth_client_secret'),
+        redirect_uri="")
+      r.login(cfg.get('reddit', 'username'), cfg.get('reddit', 'password'))
+      break
+    except Exception as e:
+      print "ERROR: {0}".format(e)
 
-  print "updating sidebar"
-  sidebar_md = template_md.replace("{{events}}", events_md)
-  subreddit.edit_wiki_page("config/sidebar", sidebar_md)
+  while True:
+    try:
+      print "fetching calendar data"
+      events_md = ""
+      for event in get_upcoming_events(cfg.get('calendar', 'id'), cfg.get('google', 'token')):
+        events_md += "* %s  \n  %s\n\n" % (format_timestamp(event), format_event(event))
 
-  print "sleeping for 60 seconds"
-  time.sleep(60)
+      print "fetching sidebar template"
+      subreddit = r.get_subreddit(cfg.get('reddit', 'subreddit'))
+      template_md = h.unescape(subreddit.get_wiki_page("sidebar").content_md)
+
+      print "updating sidebar"
+      sidebar_md = template_md.replace("{{events}}", events_md)
+      subreddit.edit_wiki_page("config/sidebar", sidebar_md)
+
+      print "sleeping for 60 seconds"
+      time.sleep(60)
+    except KeyboardInterrupt:
+      raise
+    except Exception as e:
+      print "ERROR: {0}".format(e)
+
+if __name__ == '__main__':
+  main()
